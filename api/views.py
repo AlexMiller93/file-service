aaf9a2a07a532e11ee2d3ef69863d07c2b0ef0f3
AlexMiller93 <alexandr.miller93@gmail.com>
2024-02-02 15:00:57 +0300

    add celery

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 
 from api.models import File
 from api.serializers import FileSerializer
+from api.tasks import process_file
 
 
 class FileUploadView(APIView):
@@ -11,6 +12,7 @@ class FileUploadView(APIView):
         file_serializer = FileSerializer(data=request.data)
         if file_serializer.is_valid():
             file_serializer.save()
+            process_file.delay(file_serializer.instance.id)
             return Response(file_serializer.data, status=status.HTTP_201_CREATED)
         return Response(file_serializer.data, status=status.HTTP_400)
         
